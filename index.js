a304dc755a72f05e6656edb52a815be7849c25af
kd8lvt <redunderpant@gmail.com>
2019-01-20 15:24:36 -0500

    working ws server and client

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const wsPort = 8081;
+
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({port:wsPort});
 
 app.use(express.static("public"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+wss.on('connection',(ws) => {
+	ws.on('message',(msg)=> {
+		console.log('Got a message from the client: '+msg);
+		ws.send(msg);
+	});
+});
