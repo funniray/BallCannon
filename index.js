de8504e5994dafe0f866cfd7a16fa89e409a2341
funniray <funniray10@gmail.com>
2019-01-20 16:36:07 -0500

    Added uart functions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
+const raspi = require('raspi');
+const Serial = require('raspi-serial').Serial;;
 
 const app = express();
 
@@ -13,9 +15,34 @@ app.use(express.static("public"));
 
 server.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+raspi.init(() => {
+    var serial = new Serial();
+    serial.open(() => {
+        serial.on('data', (data) => {
+            switch (data[0]) {
+                case 's':
+                    //Do nothing I don't know what to do
+                    break;
+                default:
+                    wss.clients.forEach(function each(client) {
+                        if (client !== ws && client.readyState === WebSocket.OPEN) {
+                            client.send(data);
+                        }
+                    });
+            }
+        });
+        wss.on('connection',(ws) => {
+            ws.on('message',(msg)=> {
+                serial.write('p'+msg);
+            });
+        });
+    });
+});
+
+
 wss.on('connection',(ws) => {
-	ws.on('message',(msg)=> {
-		console.log('Got a message from the client: '+msg);
-		ws.send(msg);
-	});
+    ws.on('message',(msg)=> {
+        console.log('Got a message from the client: '+msg);
+        ws.send(msg);
+    });
 });
